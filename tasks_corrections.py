f899b39944735adf0afcc01025aafd6c0fd25887
OctopusHugz <colsonscott53@gmail.com>
2020-10-02 20:24:57 +0000

    Updating outline with hardcoded correction result ID

--- tasks_corrections.py
+++ tasks_corrections.py
@@ -6,39 +6,41 @@ from os import path
 from GitCheck_settings import auth
 
 
-project_id = pid_from_cwd()
-task_url = 'https://intranet.hbtn.io/projects/{}.json?auth_token={}'.format(
-    project_id, auth)
+# project_id = pid_from_cwd()
+task_url = 'https://intranet.hbtn.io/projects/333.json?auth_token={}'.format(
+    auth)
 payload = {'Content-Type': 'application/json'}
 project = requests.get(task_url, params=payload).json()
 tasks = project.get('tasks')
-print(tasks)
-# mand_tasks = {}
-# adv_tasks = {}
-# correction_res_list = []
-# checks_dict = {}
-# count = 0
-# for task in tasks:
-#     task_id = task.get('id')
-#     task_file = task.get('github_file')
-#     if task.get('position') < 100:
-#         mand_tasks.update({task_id: task_file})
-#     else:
-#         adv_tasks.update({task_id: task_file})
-# for t_id in mand_tasks.keys():
-#     correction_url = 'https://intranet.hbtn.io/tasks/{}/start_correction.json?auth_token={}'.format(
-#         t_id, auth_token)
-#     correction_res_id = requests.post(
-#         correction_url, params=payload).json().get('id')
-#     correction_res_list.append(correction_res_id)
-# for cor_id in correction_res_list:
-#     correction_result_url = 'https://intranet.hbtn.io/correction_requests/{}.json?auth_token={}'.format(cor_id,
-#                                                                                                         auth_token)
-#     correction_result = requests.get(
-#         correction_result_url, params=payload).json()
-#     checks_dict.update(
-#         {task_id: correction_result.get('result_display').get('checks')})
-# print(checks_dict)
+mand_tasks = {}
+adv_tasks = {}
+correction_res_list = []
+checks_dict = {}
+count = 0
+for task in tasks:
+    task_id = task.get('id')
+    task_file = task.get('github_file')
+    if task.get('position') < 100:
+        mand_tasks.update({task_id: task_file})
+    else:
+        adv_tasks.update({task_id: task_file})
+for t_id in mand_tasks.keys():
+    correction_url = 'https://intranet.hbtn.io/tasks/{}/start_correction.json?auth_token={}'.format(
+        t_id, auth)
+    correction_res_id = requests.post(
+        correction_url, params=payload).json().get('id')
+    correction_res_list.append(correction_res_id)
+for cor_id in correction_res_list:
+    # correction_result_url = 'https://intranet.hbtn.io/correction_requests/{}.json?auth_token={}'.format(
+    #     cor_id, auth)
+    correction_result_url = 'https://intranet.hbtn.io/correction_requests/3591396.json?auth_token={}'.format(
+        auth)
+    correction_result = requests.get(
+        correction_result_url, params=payload).json()
+    tid = correction_result.get('task_id')
+    checks = correction_result.get('result_display').get('checks')
+    checks_dict.update({tid: checks})
+print(checks_dict)
 # for check in checks_list:
 #     label = check.get('check_label')
 #     passed = check.get('passed')
