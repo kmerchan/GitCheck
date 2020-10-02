1256110c6f0f17c9333350433036228319d1dbbe
scan3ls <scan3ls@gmail.com>
2020-10-02 19:51:59 +0000

    Update

--- github.py
+++ github.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+from sys import argv
 try:
     import git
 except ImportError as err:
@@ -32,28 +33,27 @@ def get_commit_msg():
 
 def get_files(args):
     """ get list of files to commit """
-    from sys import argv
-
-    files = args[2:]
+    files = args[1:]
     if files == []:
         print("No files given")
         exit()
     if 'README.md' not in files:
         files.append('README.md')
+    return files
 
 
 # Define argument structure
 # assume projID file_list or directory
 def git_push():
     """ Commit and push files """
-    from projects import parent_from_cwd
+    from cwd import parent_from_cwd
 
     files = get_files(argv)
 
     # TODO: change path to return of a funcion call
     path = '~/{}'.format(parent_from_cwd())
     repo = git.Repo(path)
-    result, files = get_changes(repo)
+    result, diff = get_changes(repo)
     if result is False:
         print("No Files to commit")
         exit()
@@ -65,8 +65,10 @@ def git_push():
     try:
         repo.index.add(files)
     except FileNotFoundError as err:
-        print(err)
+        print("files", files)
+        print("Add error", err)
         exit()
 
     repo.index.commit(msg)
+    repo.remotes.origin.pull()
     repo.remotes.origin.push()
