ad015d42379fd4db57ed7f5cfc3d5b9f4ae67631
OctopusHugz <colsonscott53@gmail.com>
2020-10-02 21:32:41 +0000

    Fixed import in gitcheck.py. Removed git strings from searching.py and changed w to w+ in open if file doesn't exist.

--- gitcheck.py
+++ gitcheck.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from github import git_push
 from searching import test_auth
-from tasks_corrections import request_correction
+from task_corrections import request_correction
 
 if __name__ == "__main__":
     test_auth()
