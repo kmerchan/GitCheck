853e00f4c2603a40c0e41e3db63ac479a12b104b
Kelsie Merchant <eislek02@gmail.com>
2020-10-02 19:24:46 -0500

    Fix shebang and add sudo for installs

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
-#!/bin/env bash
-# script to install dependencies and run GitCheck program
-python -m pip install gitpython
-python -m pip install gitdb
-python -m pip install requests
-# TODO: run our program which calls searching.py, github.py, and checker.py
+#!/usr/bin/env bash
+# script to install dependencies to run GitCheck program
+sudo python -m pip install gitpython
+sudo python -m pip install gitdb
+sudo python -m pip install requests
